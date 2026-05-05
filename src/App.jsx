f6d886a013c94211f786cf4cc2a791802aad457b
trungthuat2711 <ntt27112005@gmail.com>
2026-05-05 22:29:00 +0700

    dark_light_p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightness from '@mui/icons-material/SettingsBrightness'
 
-function ModeToggle() {
+function ModeSelect() {
   const { mode, setMode } = useColorScheme()
+
+  function handleChange(event) {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      { mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value={'light'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small'/> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small'/> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightness fontSize='small'/> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
@@ -19,10 +51,8 @@ function App() {
   return (
     <>
       <div>thuathayho</div>
-      <Typography variant='body2' color="text.secondary">
-        Typography
-      </Typography>
-      <ModeToggle/>
+      <hr/>
+      <ModeSelect/>
     </>
   )
 }
