a0cb3c0f996025ae1cf6ac81cbd0a337f5354239
trungthuat2711 <ntt27112005@gmail.com>
2026-05-08 15:30:56 +0700

    app bar p1

---
+++ src/components/AppBar/Menus/Templates.jsx
@@ -0,0 +1,77 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Fade from '@mui/material/Fade'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Check from '@mui/icons-material/Check'
+import { useState } from 'react'
+
+function Templates() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Button
+        id="fade-button-templates"
+        aria-controls={open ? 'fade-menu-templates' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open}
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        Templates
+      </Button>
+      <Menu
+        id="fade-menu-templates"
+        slotProps={{
+          list: {
+            'aria-labelledby': 'fade-button-templates'
+          }
+        }}
+        slots={{ transition: Fade }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <ListItemText inset>Single</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemText inset>1.15</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemText inset>Double</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Check />
+          </ListItemIcon>
+          Custom: 1.2
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemText>Add space before paragraph</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemText>Add space after paragraph</ListItemText>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemText>Custom spacing…</ListItemText>
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Templates
---
+++ src/components/AppBar/Menus/Workspaces.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Typography from '@mui/material/Typography'
+import ContentCut from '@mui/icons-material/ContentCut'
+import ContentCopy from '@mui/icons-material/ContentCopy'
+import ContentPaste from '@mui/icons-material/ContentPaste'
+import Divider from '@mui/material/Divider'
+import Cloud from '@mui/icons-material/Cloud'
+import Fade from '@mui/material/Fade'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+
+function Workspaces() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Button
+        id="fade-button-workspaces"
+        aria-controls={open ? 'fade-menu-workspaces' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open}
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        Workspaces
+      </Button>
+      <Menu
+        id="fade-menu-workspaces"
+        slotProps={{
+          list: {
+            'aria-labelledby': 'fade-button-workspaces'
+          }
+        }}
+        slots={{ transition: Fade }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ContentCut fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Cut</ListItemText>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            ⌘X
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ContentCopy fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Copy</ListItemText>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            ⌘C
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ContentPaste fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Paste</ListItemText>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            ⌘V
+          </Typography>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Cloud fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Web Clipboard</ListItemText>
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Workspaces
