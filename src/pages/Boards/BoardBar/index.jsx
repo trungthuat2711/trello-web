00cba2ebf8cc1850220da5c7622d53fa7a40e9a6
trungthuat2711 <ntt27112005@gmail.com>
2026-05-11 23:43:14 +0700

    trello_board_content

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -106,7 +106,12 @@ function BoardBar() {
               width: 34,
               height: 34,
               fontSize: 16,
-              border: 'none'
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': {
+                backgroundColor: '#a4b0be'
+              }
             }
           }}
         >
